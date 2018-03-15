c4df3ab06b0d9e53fb732768ad9d7603fd9ec1b5
ComputerCode00 <Just_chill8@outlook.com>
2018-03-14 19:47:23 -0500

    attempting to resolve any content before i push to Jose branch

--- client/src/Components/News.js
+++ client/src/Components/News.js
@@ -4,9 +4,15 @@ import './News.css'
 export default class News extends Component {
     render () {
         return (
-            <div>
-                World Map
-            </div>
+            <div className="container" id="nav" style="background-color: grey;">
+  <div className="topnav" id="myTopnav" style="font-size: 20px; background-color: red; color: red; float:right; opacity:0.6">
+      <a href="/first.html" className="active">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+      <a href="javascript:void(0);" style="font-size:15px;" className="icon" onclick="myFunction()">&#9776;</a>
+    </div>
+</div>
         )
     }
 }
