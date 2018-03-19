d9a7c9bd899d98266cce39fb28926b4cf55b0c93
J <J21>
2018-03-18 22:39:16 -0500

    Minor fixes, but a lot of things work now

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import News from './Components/News';
+import Save from './Components/Save';
+import Article from './Components/Article'
+import Login from './Components/Login';
 import Dev from './Components/Dev';
 import Navbar from './Components/CustomNavbar';
 
@@ -14,7 +17,9 @@ class App extends Component {
         <div>
           <Navbar />
           <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
           <Route exact path='/news' component={News} />
+          <Route exact path='/article' component={Article} />
           <Route exact path='/dev' component={Dev} />
         </div>
       </Router>
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Route, NavLink, HashRouter} from "react-router-dom";
+import Login from './Login.js';
 import './Home.css';
 
 export default class Home extends Component {
@@ -11,23 +13,12 @@ export default class Home extends Component {
                     {/* <Jumbotron> */}
                         <h2>Welcome</h2>
                         <p>News and Weather from Cities around the Globe</p>
-                    {/* </Jumbotron> */}
-
                 {/* spinning globe */}
                 <div className="circle">
                     <div class="globe"></div>
                     </div>
-
                     <br/>
-                    <Link to= "/#">
-                    <Button bsStyle="primary">Sign In</Button>
-                    </Link>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <Link to= "/#">
-                    <Button bsStyle="secondary">Sign Up</Button>
-                    </Link>
+                    <Button bsStyle="primary" href = "/Login">Sign In</Button>
                 </Grid>
             </div>
         )
